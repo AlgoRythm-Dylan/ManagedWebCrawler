5578ddb164b4a3b7ab3747067b8195e1ba71233f
AlgoRythm-Dylan <dperunko@comcast.net>
2023-12-01 19:04:52 -0500

    Add HTML structures

---
+++ Lib/HTML/Element.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagedWebCrawler.Lib.HTML
+{
+    public class Element
+    {
+        public string TagName { get; set; } = "";
+        public Dictionary<string, string> Attributes { get; set; } = new();
+        public List<Element> Children { get; set; } = new();
+        public string Content = "";
+    }
+}
